22d3899d464f4029d2662830a151954a604f83bb
Albert Raset <albert.raset@institutvidreres.cat>
2021-11-26 13:46:54 +0100

    Exercici 5

--- JS_Funcions_AlbertRaset.js
+++ JS_Funcions_AlbertRaset.js
@@ -1,4 +1,8 @@
-function fnom(){
-    var nom = 'Albert'
+//Exercici 5
+function vocal(str){
+    const contar = str.match(/[aeiou]/gi).length;
+    return contar;
 }
-console.log(nom);
+const text = prompt('Introdueix un text:');
+const resultat = vocal(text);
+console.log('Vocals: ' + resultat);
